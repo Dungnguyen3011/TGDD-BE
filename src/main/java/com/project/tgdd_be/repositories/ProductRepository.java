cb54647f972595bebdb1cf1e4e5eb98696340aa1
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-06-24 18:29:54 +0700

    add code into entity and repo

--- src/main/java/com/project/tgdd_be/repositories/ProductRepository.java
+++ src/main/java/com/project/tgdd_be/repositories/ProductRepository.java
@@ -1,5 +1,10 @@
 package com.project.tgdd_be.repositories;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.tgdd_be.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
