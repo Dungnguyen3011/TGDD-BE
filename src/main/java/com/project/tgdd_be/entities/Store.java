7e7c8bcdef09712910a7909f018f659e8d978516
HaiQuang1107 <nguyenhaiquang1107@gmail.com>
2022-06-25 20:00:51 +0700

    Store

---
+++ src/main/java/com/project/tgdd_be/entities/Store.java
@@ -0,0 +1,31 @@
+package com.project.tgdd_be.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="tbl_Store")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Store {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="store_id", nullable = false, updatable = false)
+	private String storeId;
+	
+	@Column(name="store_name")
+	private String StoreName;
+	
+
+	
+
+}
---
+++ src/main/java/com/project/tgdd_be/repositories/StoreRepository.java
@@ -0,0 +1,15 @@
+package com.project.tgdd_be.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.tgdd_be.entities.Store;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+}
+
+
+
+
+
+
+
+
