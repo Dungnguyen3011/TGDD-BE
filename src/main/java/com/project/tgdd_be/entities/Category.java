d3917cbefbea015f96dd51831a662d9e4eadfb0f
Koi1420 <khoikad@gmail.com>
2022-06-26 11:54:24 +0700

    Create Category.java

---
+++ src/main/java/com/project/tgdd_be/entities/Category.java
@@ -0,0 +1,28 @@
+package com.project.tgdd_be.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name="Categories")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="category_id", nullable = false, updatable = false)
+	private int categoryId;
+	
+	@Column(name="category_name")
+	private String categoryName;
+	
+}
