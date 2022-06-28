76f2ef060f508e01120793e5bd2b0a59b08d5877
tuanngoc2510 <102311792+tuanngoc2510@users.noreply.github.com>
2022-06-28 15:21:02 +0700

    store service

--- src/main/java/com/project/tgdd_be/repositories/StoreRepository.java
+++ src/main/java/com/project/tgdd_be/repositories/StoreRepository.java
@@ -2,7 +2,7 @@ package com.project.tgdd_be.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.tgdd_be.entities.Store;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
+public interface StoreRepository extends JpaRepository<Store, Integer> {
 
 }
 
