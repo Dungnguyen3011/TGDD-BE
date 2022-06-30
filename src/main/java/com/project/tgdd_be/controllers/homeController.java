743cd1c24c3dea5c1421a7529aa0711736e225d7
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-06-30 14:26:59 +0700

    fix pom be add web

--- src/main/java/com/project/tgdd_be/controllers/homeController.java
+++ src/main/java/com/project/tgdd_be/controllers/homeController.java
@@ -1,5 +1,23 @@
 package com.project.tgdd_be.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.tgdd_be.entities.Product;
+import com.project.tgdd_be.service.ProductService;
+
+@RestController
 public class homeController {
 
+	@Autowired
+	private ProductService sv;
+	
+	@GetMapping("/api/product")
+	public List<Product> getAll(){
+		List<Product> pr= sv.listAll();
+		return pr;
+	}
 }
