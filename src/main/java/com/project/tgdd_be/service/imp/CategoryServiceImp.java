826c1e016ac557769544544a3dbde8b04fb0c9e7
Koi1420 <khoikad@gmail.com>
2022-06-30 13:20:55 +0700

    Create CategoryServiceImp.java

---
+++ src/main/java/com/project/tgdd_be/service/imp/CategoryServiceImp.java
@@ -0,0 +1,34 @@
+package com.project.tgdd_be.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.tgdd_be.entities.Category;
+import com.project.tgdd_be.repositories.CategoryRepository;
+import com.project.tgdd_be.service.CategoryService;
+
+public class CategoryServiceImp implements CategoryService{
+	@Autowired
+	private CategoryRepository categoryRep;
+	
+	@Override
+	public List<Category> listAll() {
+		return categoryRep.findAll();
+	}
+	
+	@Override
+	public void save(Category cat) {
+		categoryRep.save(cat);
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		categoryRep.deleteById(id);
+	}
+	
+	@Override
+	public Category getCategorByID(Integer id) {
+		return categoryRep.findById(id).get();	
+	}
+}
