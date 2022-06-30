5a37e4cb37fd3f0623339c5f528eefaa1415e22a
Koi1420 <khoikad@gmail.com>
2022-06-30 13:20:51 +0700

    Update CategoryService.java

--- src/main/java/com/project/tgdd_be/service/CategoryService.java
+++ src/main/java/com/project/tgdd_be/service/CategoryService.java
@@ -2,31 +2,15 @@ package com.project.tgdd_be.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.project.tgdd_be.entities.Category;
-import com.project.tgdd_be.repositories.CategoryRepository;
 
-@Service
-public class CategoryService {
-	@Autowired
-	private CategoryRepository categoryRep;
-	
-	public List<Category> listAll() {
-		return categoryRep.findAll();
-	}
-	
-	public void save(Category cat) {
-		categoryRep.save(cat);
-	}
-	
-	
-	public void delete(Integer id) {
-		categoryRep.deleteById(id);
-	}
-	
-	public Category get(Integer id) {
-		return categoryRep.findById(id).get();	
-	}
+public interface CategoryService {
+
+	public List<Category> listAll();
+
+	public void save(Category cat);
+
+	public void delete(Integer id);
+
+	public Category getCategorByID(Integer id);
 }
