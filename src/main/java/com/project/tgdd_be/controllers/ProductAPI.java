d02d66609d2616d214bc548e717a2406ebcc0941
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-04 18:37:20 +0700

    Update ProductAPI.java

--- src/main/java/com/project/tgdd_be/controllers/ProductAPI.java
+++ src/main/java/com/project/tgdd_be/controllers/ProductAPI.java
@@ -7,8 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.tgdd_be.entities.Product;
@@ -54,7 +54,7 @@ public class ProductAPI {
 		return ResponseEntity.ok(pr);
 	}
 	
-	@GetMapping("/search/{id}")
+	@PutMapping("/search/{id}")
 	public ResponseEntity<?> updateProduct(@PathVariable Integer id, @RequestBody Product product){
 		Product updateProudct = sv.getProductbyID(id);
 		updateProudct.setProductName(product.getProductName());
