73f57e07076b04f440ba4ce687589960412dc22d
Koi1420 <khoikad@gmail.com>
2022-07-04 17:41:38 +0700

    Update CategoryRepository.java

--- src/main/java/com/project/tgdd_be/repositories/CategoryRepository.java
+++ src/main/java/com/project/tgdd_be/repositories/CategoryRepository.java
@@ -1,10 +1,21 @@
 package com.project.tgdd_be.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.project.tgdd_be.entities.Category;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	
+	@Query(value ="SELECT * FROM tbl_categories p WHERE "+
+			"p.status = 'TRUE' ", nativeQuery = true)
+	List<Category> listCateForCus();
+	
+	@Query(value ="SELECT * FROM tbl_categories p WHERE "+
+			"p.status = 'false' ", nativeQuery = true)
+	List<Category> deleteCategory();
 
 }
