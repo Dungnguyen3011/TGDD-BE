e13a0e4595a90a9c10c319aaed9ea42a12ebf07a
Toan <toanddse140564@fpt.edu.vn>
2022-07-04 10:20:08 +0700

    fix OrderAPI
    test api

--- src/main/java/com/project/tgdd_be/controllers/OrderAPI.java
+++ src/main/java/com/project/tgdd_be/controllers/OrderAPI.java
@@ -1,36 +1,51 @@
 package com.project.tgdd_be.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.tgdd_be.entities.Order;
-import com.project.tgdd_be.entities.Product;
+import com.project.tgdd_be.repositories.OrderRepository;
 import com.project.tgdd_be.service.OrderService;
 
 @RestController
 public class OrderAPI {
 	@Autowired
 	private OrderService os;
+	@Autowired
+	private OrderRepository or;
 
 	@GetMapping("/api/order")
 	public ResponseEntity<?> getAll() {
 		List<Order> lo = os.listAll();
-		return ResponseEntity.ok(os);
+		return ResponseEntity.ok(lo);
 
 	}
 	@GetMapping("/api/orderBySpecificPhone")
-	public ResponseEntity<?> getOrderBySpecificPhone(@RequestParam("phone_number") String phone_number){
-		List<Order> lo = os.listOrderBySpecificPhone(phone_number);
-		return ResponseEntity.ok(os);
+	public ResponseEntity<?> getOrderBySpecificPhone(@RequestParam(required=false,name="query") String query){
+		/* List<Order> lo = os.listOrderBySpecificPhone(query); */
+		return ResponseEntity.ok(os.listOrderBySpecificPhone(query));
 	}
-	@PostMapping("/api/product")
-	public Order updateOrderStatus(Order order) {
+	@GetMapping("/api/updateOrderStatus")
+	public Order updateOrderStatus(ModelMap model,@ModelAttribute("order")Order order) {
+		Optional<Order> opOrder = or.findById(order.getOrderId());
+		if (opOrder.isPresent()) {
+			//update
+			order.setStatus(true);
+			
+		}else {
+			//not exist
+			return null;
+		}
 		return os.updateOrderStatus(order);
 	}
 }
--- src/main/java/com/project/tgdd_be/service/imp/OrderServiceImp.java
+++ src/main/java/com/project/tgdd_be/service/imp/OrderServiceImp.java
@@ -3,20 +3,21 @@ package com.project.tgdd_be.service.imp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.project.tgdd_be.entities.Order;
 import com.project.tgdd_be.repositories.OrderRepository;
 import com.project.tgdd_be.service.OrderService;
 
+@Service
 public class OrderServiceImp implements OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
+
 	@Override
 	public List<Order> listAll() {
 		return orderRepository.findAll();
 	}
-
-	
 
 	@Override
 	public void delete(Integer id) {
@@ -29,15 +30,12 @@ public class OrderServiceImp implements OrderService {
 	}
 
 	@Override
-	public List<Order> listOrderBySpecificPhone(String phone_number) {
-		return orderRepository.listOrderBySpecificPhone(phone_number);
+	public List<Order> listOrderBySpecificPhone(String query) {
+		return orderRepository.listOrderBySpecificPhone(query);
 	}
-
-	
 
 	@Override
 	public Order save(Order order) {
-		// TODO Auto-generated method stub
 		return orderRepository.save(order);
 	}
 
@@ -45,6 +43,5 @@ public class OrderServiceImp implements OrderService {
 	public Order updateOrderStatus(Order order) {
 		return orderRepository.save(order);
 	}
-	
 
 }
