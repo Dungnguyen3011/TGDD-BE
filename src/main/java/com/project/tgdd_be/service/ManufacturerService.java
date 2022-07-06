0e9fb33a8c9f1c70c5979c760358a965d374cb3d
HaiQuang1107 <nguyenhaiquang1107@gmail.com>
2022-07-06 17:16:50 +0700

    update code manufaturer list all, list forcus

--- src/main/java/com/project/tgdd_be/service/ManufacturerService.java
+++ src/main/java/com/project/tgdd_be/service/ManufacturerService.java
@@ -3,16 +3,17 @@ package com.project.tgdd_be.service;
 import java.util.List;
 
 import com.project.tgdd_be.entities.Manufacturer;
+import com.project.tgdd_be.model.dto.ManufaturerDTO;
 
 public interface ManufacturerService {
 	
-	public List<Manufacturer> listAll();
+	public List<ManufaturerDTO> listAll();
 	
 	public Manufacturer save(Manufacturer Manufacturer);
 	
 	public void delete(Integer id);
 	
-	public Manufacturer getManufacturerbyID(Integer id);
-	public List<Manufacturer> listManufacturerForCus();
+	public ManufaturerDTO getManufacturerbyID(Integer id);
+	public List<ManufaturerDTO> listManufacturerForCus();
 	public void updateMStatus(Integer[] ids);
 }
