9986a88214b4ce96af9cc79e5b3e20a3767c171c
tuanngoc2510 <102311792+tuanngoc2510@users.noreply.github.com>
2022-07-08 00:24:39 +0700

    update store

--- src/main/java/com/project/tgdd_be/service/StoreService.java
+++ src/main/java/com/project/tgdd_be/service/StoreService.java
@@ -3,17 +3,18 @@ package com.project.tgdd_be.service;
 import java.util.List;
 
 import com.project.tgdd_be.entities.Store;
+import com.project.tgdd_be.model.dto.StoreDTO;
 
 public interface StoreService {
 
-	public List<Store> listAll();
+	public List<StoreDTO> listAll();
 
 	public Store save(Store sto);
 
 	public void delete(Integer id);
 
-	public Store getStorebyID(Integer id);
+	public StoreDTO getStorebyID(Integer id);
 
-	public List<Store> listStoreFindByLocation(Integer id);
+	public List<StoreDTO> listStoreFindByLocation(Integer id);
 
 }
