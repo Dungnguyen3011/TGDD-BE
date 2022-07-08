44b9a6313d414249dbbb35f80ba3bbd1d3d0f768
Diu Huong <87648894+MaoMaokongu@users.noreply.github.com>
2022-07-08 09:26:17 +0700

    LocationDTO

--- src/main/java/com/project/tgdd_be/service/LocationService.java
+++ src/main/java/com/project/tgdd_be/service/LocationService.java
@@ -3,6 +3,7 @@ package com.project.tgdd_be.service;
 import java.util.List;
 
 import com.project.tgdd_be.entities.Location;
+import com.project.tgdd_be.model.dto.LocationDTO;
 
 public interface LocationService {
 	
@@ -14,6 +15,5 @@ public interface LocationService {
 
 	public Location getLocationbyID(Integer id);
 	
-	
-	
+	public LocationDTO getLocationDTObyId(Integer id);
 }
