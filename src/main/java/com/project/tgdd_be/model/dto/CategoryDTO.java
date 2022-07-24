b7ea8519588a7fdbfa8bb84569fd68955cfe71d4
Koi1420 <khoikad@gmail.com>
2022-07-24 22:31:26 +0700

    valid category

--- src/main/java/com/project/tgdd_be/model/dto/CategoryDTO.java
+++ src/main/java/com/project/tgdd_be/model/dto/CategoryDTO.java
@@ -1,5 +1,7 @@
 package com.project.tgdd_be.model.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,8 @@ public class CategoryDTO {
 	
 	private Integer categoryId;
 	
+	@NotBlank(message = "Category name can not be empty")	
+	@Size(min = 2, message = "Name should have at least 2 characters" )
 	private String categoryName;
 	
 	private Boolean status;
