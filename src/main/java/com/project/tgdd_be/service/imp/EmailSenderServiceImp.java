09f057f8f82e188ef8d63dfae588f25c0b836fb4
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-25 12:07:50 +0700

    add send email function

---
+++ src/main/java/com/project/tgdd_be/service/imp/EmailSenderServiceImp.java
@@ -0,0 +1,42 @@
+package com.project.tgdd_be.service.imp;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.project.tgdd_be.service.EmailSenderService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmailSenderServiceImp implements EmailSenderService {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(EmailSenderServiceImp.class);
+
+	private final JavaMailSender mailSender;
+	
+	@Override
+	@Async
+	public void send(String to, String email) {
+		try {
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+			helper.setText(email, true);
+			helper.setTo(to);
+			helper.setFrom("tgddspringgroup2@gmail.com");
+			helper.setSubject("CONFIRMING LETTER FOR YOUR ORDER");
+			mailSender.send(mimeMessage);
+		}catch(MessagingException e) {
+			LOGGER.error("failed to send email", e);
+			throw new IllegalStateException("failed to send email");
+		}
+	}
+
+}
