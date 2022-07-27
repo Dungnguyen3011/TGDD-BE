0a1b440e7ece88e1a97cf44d7993103a6ef820b7
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-27 21:58:17 +0700

    fix login

--- src/main/java/com/project/tgdd_be/security/SecurityConfig.java
+++ src/main/java/com/project/tgdd_be/security/SecurityConfig.java
@@ -10,7 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        .antMatchers("/api/admin")
+        .antMatchers("/api/admin/**")
         .authenticated()
         .and()
             .formLogin()
