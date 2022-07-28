4bcf6b4ed9128ef7cd6f9f0fe58733fd76913688
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-28 10:12:30 +0700

    7.4

--- src/main/java/com/project/tgdd_be/controllers/AdminAPI.java
+++ src/main/java/com/project/tgdd_be/controllers/AdminAPI.java
@@ -1,18 +1,24 @@
 package com.project.tgdd_be.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminAPI {
 	
+	
 	@GetMapping("/api/admin")
-	public String admin() {
-		return "https://react-tgdd.vercel.app/admin";
+	public void admin(HttpServletResponse res) throws IOException {
+		res.sendRedirect("https://react-tgdd.vercel.app/admin");
 	}
 
-	@GetMapping("/api/home")
-	public String home() {
-		return "https://react-tgdd.vercel.app/";
-	}
+//	@GetMapping("/api/home")
+//	public String home() {
+//		URI uri = https://react-tgdd.vercel.app/admin;
+//		return  Response.temporaryRedirect(uri).build();
+//	}
 }
