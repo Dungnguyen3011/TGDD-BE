70dfbfc19caf60b4a0314bb278a5eea080df668f
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-29 00:58:37 +0700

    done delete cate, manu, store -> delete Product fix 1.0

--- src/main/java/com/project/tgdd_be/controllers/CategoryAPI.java
+++ src/main/java/com/project/tgdd_be/controllers/CategoryAPI.java
@@ -12,10 +12,15 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.project.tgdd_be.entities.Category;
+import com.project.tgdd_be.entities.Manufacturer;
 import com.project.tgdd_be.entities.Product;
+import com.project.tgdd_be.entities.Store;
 import com.project.tgdd_be.model.dto.CategoryDTO;
+import com.project.tgdd_be.model.dto.ProductDTO;
 import com.project.tgdd_be.service.CategoryService;
+import com.project.tgdd_be.service.ManufacturerService;
 import com.project.tgdd_be.service.ProductService;
+import com.project.tgdd_be.service.StoreService;
 
 @RestController
 public class CategoryAPI {
@@ -26,9 +31,25 @@ public class CategoryAPI {
 	@Autowired
 	private ProductService sv;
 	
+	@Autowired
+	private ManufacturerService Msv;
+
+	@Autowired
+	private StoreService Ssv;
+	
 	public Category dtoToCategory(CategoryDTO cateDTO) {
 		Category cate = new Category(cateDTO.getCategoryId(), cateDTO.getCategoryName(),cateDTO.getStatus());
 		return cate;
+	}
+	
+	public Product dtotoProduct(ProductDTO productdto) {
+		Manufacturer manu = Msv.getManufacturerbyID2(productdto.getManufacturerId());
+		Category cate = cs.getCategorByID(productdto.getCategoryId());
+		Store sto = Ssv.getStorebyID(productdto.getStoreId());
+		Product pro = new Product(productdto.getProductId(), productdto.getProductName(), productdto.getQuantity(),
+				productdto.getUnitPrice(), productdto.getSalePrice(), productdto.getDescription(), productdto.getRate(),
+				productdto.getStatus(), productdto.getImage(), cate, manu, sto);
+		return pro;
 	}
 //	public Category dtoToStatusCategory(CategoryDTO cateDTO) {
 //		Category cate = new Category(cateDTO.getCategoryId(),cateDTO.getStatus());
@@ -67,9 +88,10 @@ public class CategoryAPI {
 		CategoryDTO cateDTO = cs.findCategoryById(id);
 		Category cate = dtoToCategory(cateDTO);
 		cate.setStatus(false);
-		for(Product product : sv.listProductByCategory(id)) {
-			product.setStatus(false);
-			sv.save(product);
+		for(ProductDTO dto : sv.listProductDTOByCategory(id)) {
+			Product pro = dtotoProduct(dto);
+			pro.setStatus(false);
+			sv.save(pro);
 		}
 		return ResponseEntity.ok(cs.save(cate));
 	}
