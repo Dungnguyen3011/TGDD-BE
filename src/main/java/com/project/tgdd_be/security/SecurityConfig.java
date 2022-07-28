1102c7160756ad7bbb5fe5519fc864759e7ea6ef
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-07-28 11:18:56 +0700

    7.9

--- src/main/java/com/project/tgdd_be/security/SecurityConfig.java
+++ src/main/java/com/project/tgdd_be/security/SecurityConfig.java
@@ -1,28 +1,28 @@
-//package com.project.tgdd_be.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//        .antMatchers("/api/admin/**")
-//        .authenticated()
-//        .and()
-//            .formLogin()
-//                .defaultSuccessUrl("/api/admin")
-//                .permitAll()
-//            .and()
-//            .logout()
-//            .logoutSuccessUrl("/api/home").permitAll()
-//            .and()
-//            .csrf().disable();
-//
-//    }
-//}
+package com.project.tgdd_be.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/api/admin/**")
+        .authenticated()
+        .and()
+            .formLogin()
+                .defaultSuccessUrl("/api/admin")
+                .permitAll()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/api/home").permitAll()
+            .and()
+            .csrf().disable();
+
+    }
+}
